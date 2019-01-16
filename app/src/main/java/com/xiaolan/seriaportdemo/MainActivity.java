72dcd5b2db8200305158a173c9ae7451b4a5b0d4
zhangbao <15056006309@163.com>
2019-01-16 15:06:35 +0800

    串口助手demo

---
+++ app/src/main/java/com/xiaolan/seriaportdemo/MainActivity.java
@@ -0,0 +1,293 @@
+package com.xiaolan.seriaportdemo;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import android_serialport_api.SerialPortFinder;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, SerialPortHelper.OnDataReceivedListener {
+    private static final String TAG = "MainActivity";
+    @BindView(R.id.btn_set_port)
+    Button mBtnSetPort;
+    @BindView(R.id.btn_set_baud)
+    Button mBtnSetBaud;
+    @BindView(R.id.btn_set_data)
+    Button mBtnSetData;
+    @BindView(R.id.btn_set_stop)
+    Button mBtnSetStop;
+    @BindView(R.id.btn_set_verify)
+    Button mBtnSetVerify;
+    @BindView(R.id.cb_hex)
+    RadioButton mCbHex;
+    @BindView(R.id.cb_text)
+    RadioButton mCbText;
+    @BindView(R.id.rg)
+    RadioGroup mRg;
+    @BindView(R.id.et)
+    EditText mEt;
+    @BindView(R.id.btn_send)
+    Button mBtnSend;
+    @BindView(R.id.btn_open_port)
+    Button mBtnOpenPort;
+    @BindView(R.id.btn_clear)
+    Button mBtnClear;
+    @BindView(R.id.cb_hex_rev)
+    RadioButton mCbHexRev;
+    @BindView(R.id.cb_text_rev)
+    RadioButton mCbTextRev;
+    @BindView(R.id.rg_rev)
+    RadioGroup mRgRev;
+    @BindView(R.id.editTextRecDisp)
+    EditText mEditTextRecDisp;
+
+
+    private MyHandler mMyHandler;
+    private int sendType = 0;
+    private int revType = 0;
+    private DispQueueThread mDispQueueThread;
+    private SerialPortFinder mPortFinder;
+    private SerialPortHelper mPortHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        KeybordUtils.isSoftInputShow(this);
+        mEt.clearFocus();
+
+        mCbHex.setChecked(true);
+        mCbHexRev.setChecked(true);
+
+        mRg.setOnCheckedChangeListener(this);
+        mRgRev.setOnCheckedChangeListener(this);
+        mMyHandler = new MyHandler();
+        mDispQueueThread = new DispQueueThread();
+        mDispQueueThread.start();
+        mPortFinder = new SerialPortFinder();
+        mPortHelper = new SerialPortHelper();
+        mPortHelper.setOnDataReceivedListener(this);
+    }
+
+    @OnClick({R.id.btn_set_port, R.id.btn_set_baud, R.id.btn_set_data, R.id.btn_set_stop,
+            R.id.btn_set_verify, R.id.btn_send, R.id.btn_open_port,
+            R.id.btn_clear})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_set_port:
+                new AlertDialog.Builder(this)
+                        .setTitle("选择串口")//设置对话框标题
+                        .setIcon(android.R.drawable.ic_menu_info_details)//设置对话框图标
+                        .setSingleChoiceItems(mPortFinder.getAllDevices(), 0, (dialog, which) -> {
+                            String device = mPortFinder.getAllDevicesPath()[which];
+                            mBtnSetPort.setText(device);
+                            mPortHelper.setPort(device);
+                            dialog.dismiss();
+                        })
+                        .show();
+                break;
+            case R.id.btn_set_baud:
+                new AlertDialog.Builder(this)
+                        .setTitle("选择波特率")//设置对话框标题
+                        .setIcon(android.R.drawable.ic_menu_info_details)//设置对话框图标
+                        .setSingleChoiceItems(mPortFinder.getBaudRateList(), 0, (dialog, which) -> {
+                            String bRate = mPortFinder.getBaudRateList()[which];
+                            Integer baudRate = Integer.valueOf(bRate);
+                            mBtnSetBaud.setText(baudRate);
+                            mPortHelper.setBaudRate(baudRate);
+                            dialog.dismiss();
+                        })
+                        .show();
+                break;
+            case R.id.btn_set_data:
+                new AlertDialog.Builder(this)
+                        .setTitle("选择数据位")//设置对话框标题
+                        .setIcon(android.R.drawable.ic_menu_info_details)//设置对话框图标
+                        .setSingleChoiceItems(mPortFinder.getDataBits(), 0, (dialog, which) -> {
+                            String dataBits = mPortFinder.getDataBits()[which];
+                            mBtnSetData.setText(dataBits);
+                            mPortHelper.setDataBits(dataBits);
+                            dialog.dismiss();
+                        })
+                        .show();
+                break;
+            case R.id.btn_set_stop:
+                new AlertDialog.Builder(this)
+                        .setTitle("选择停止位")//设置对话框标题
+                        .setIcon(android.R.drawable.ic_menu_info_details)//设置对话框图标
+                        .setSingleChoiceItems(mPortFinder.getStopBits(), 0, (dialog, which) -> {
+                            String stopBits = mPortFinder.getStopBits()[which];
+                            mBtnSetStop.setText(stopBits);
+                            mPortHelper.setStopBits(stopBits);
+                            dialog.dismiss();
+                        })
+                        .show();
+                break;
+            case R.id.btn_set_verify:
+                new AlertDialog.Builder(this)
+                        .setTitle("选择停止位")//设置对话框标题
+                        .setIcon(android.R.drawable.ic_menu_info_details)//设置对话框图标
+                        .setSingleChoiceItems(mPortFinder.getParityBits(), 0, (dialog, which) -> {
+                            String parityBits = mPortFinder.getParityBits()[which];
+                            mBtnSetVerify.setText(parityBits);
+                            mPortHelper.setParityBits(parityBits);
+                            dialog.dismiss();
+                        })
+                        .show();
+                break;
+            case R.id.btn_send:
+                if (mPortHelper != null && !mPortHelper.isOpen()) {
+                    Toast.makeText(this, "请打开串口", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String trim = mEt.getText().toString().trim();
+                if (!TextUtils.isEmpty(trim)) {
+                    if (sendType == 0) {
+                        mPortHelper.sendHex(trim);
+                    } else {
+                        mPortHelper.sendTxt(trim);
+                    }
+                } else {
+                    Toast.makeText(this, "请输入内容", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btn_open_port:
+                //打开/关闭串口
+                if (mBtnOpenPort.getText().equals("打开串口")) {
+                    try {
+                        mBtnSetPort.setText(mPortHelper.getPort());
+                        mBtnSetBaud.setText(String.valueOf(mPortHelper.getBaudRate()));
+                        mBtnSetData.setText(mPortHelper.getDataBits());
+                        mBtnSetStop.setText(mPortHelper.getStopBits());
+                        mBtnSetVerify.setText(mPortHelper.getParityBits());
+                        mPortHelper.open();
+                        mBtnOpenPort.setText("关闭串口");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        mBtnOpenPort.setText("打开串口");
+                    }
+                } else {
+                    mPortHelper.close();
+                    mBtnOpenPort.setText("打开串口");
+                }
+                break;
+            case R.id.btn_clear:
+                //清空数据接收区
+                mEditTextRecDisp.setText("");
+                count = 0;
+                mEt.clearFocus();
+                KeybordUtils.closeKeybord(mEt, this);
+                break;
+        }
+    }
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (checkedId == mCbHex.getId()) {
+            sendType = 0;
+        } else if (checkedId == mCbText.getId()) {
+            sendType = 1;
+        } else if (checkedId == mCbHexRev.getId()) {
+            revType = 0;
+        } else if (checkedId == mCbTextRev.getId()) {
+            revType = 1;
+        }
+    }
+
+    @Override
+    public void onDataReceived(ComBean comBean) {
+        runOnUiThread(() -> {
+            mDispQueueThread.AddQueue(comBean);//线程定时刷新显示
+        });
+    }
+
+    //----------------------------------------------------刷新显示线程
+    private class DispQueueThread extends Thread {
+        private Queue<ComBean> QueueList = new LinkedList<>();
+
+        @Override
+        public void run() {
+            super.run();
+            while (!isInterrupted()) {
+                final ComBean ComData;
+                while ((ComData = QueueList.poll()) != null) {
+                    runOnUiThread(() -> DispRecData(ComData));
+                    try {
+                        Thread.sleep(50);//显示性能高的话，可以把此数值调小。
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+
+        synchronized void AddQueue(ComBean ComData) {
+            QueueList.add(ComData);
+        }
+    }
+
+    private int count = 0;
+
+    //----------------------------------------------------显示接收数据
+    private void DispRecData(ComBean comBean) {
+        StringBuilder sMsg = new StringBuilder();
+        sMsg.append(comBean.sRecTime);
+        sMsg.append("[");
+        sMsg.append(comBean.sComPort);
+        sMsg.append("]");
+        if (revType == 0) {
+            sMsg.append("[Hex] ");
+            String hex = MyFunc.ByteArrToHex(comBean.bRec);
+            String[] split = hex.split(" ");
+            sMsg.append(hex);
+            sMsg.append("------->").append(count).append("------->size:").append(split.length);
+            Log.w(TAG, hex);
+        } else {
+            sMsg.append("[Text] ");
+            String text = new String(comBean.bRec);
+            sMsg.append(text);
+            sMsg.append("------->").append(count).append("------->size:").append(text.length());
+            Log.w(TAG, text);
+        }
+        count++;
+        if (comBean.mRecOrSend == 0) {
+            sMsg.append("---[收]");
+        } else if (comBean.mRecOrSend == 1) {
+            sMsg.append("---[发]");
+        }
+        sMsg.append("\r\n");
+        mEditTextRecDisp.append(sMsg);
+    }
+
+    private static class MyHandler extends Handler {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mMyHandler != null) {
+            mMyHandler.removeCallbacksAndMessages(null);
+            mMyHandler = null;
+        }
+        mPortHelper.close();
+    }
+}
---
+++ app/src/main/java/com/xiaolan/seriaportdemo/SerialPortHelper.java
@@ -0,0 +1,363 @@
+package com.xiaolan.seriaportdemo;
+
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.InvalidParameterException;
+
+import android_serialport_api.SerialPort;
+
+public class SerialPortHelper {
+
+    private static final String TAG = "SerialPortHelper";
+    private SerialPort mSerialPort;
+    private OutputStream mOutputStream;
+    private InputStream mInputStream;
+    private ReadThread mReadThread;
+    private SendThread mSendThread;
+    private String sPort = "/dev/ttyS3";
+    private int iBaudRate = 9600;
+    private String mDataBits = "8";
+    private String mStopBits = "1";
+    private String mParityBits = "N";
+    private boolean _isOpen = false;
+    private byte[] _bLoopData = new byte[]{0x30};
+    private int iDelay = 500;
+    private BufferedInputStream mBufferedInputStream;
+    private static final int SHOW_LENGTH = 30;
+
+    //----------------------------------------------------
+    public SerialPortHelper() {
+        this("/dev/ttyS3", 9600);
+    }
+
+    public SerialPortHelper(String sPort) {
+        this(sPort, 9600);
+    }
+
+    public SerialPortHelper(String sPort, String sBaudRate) {
+        this(sPort, Integer.parseInt(sBaudRate));
+    }
+
+    public SerialPortHelper(String sPort, int iBaudRate) {
+        this.sPort = sPort;
+        this.iBaudRate = iBaudRate;
+    }
+
+    //----------------------------------------------------
+    public void open() throws SecurityException, IOException, InvalidParameterException {
+//        File device = new File(sPort);
+//        //检查访问权限，如果没有读写权限，进行文件操作，修改文件访问权限
+//        if (!device.canRead() || !device.canWrite()) {
+//            try {
+//                //通过挂在到linux的方式，修改文件的操作权限
+//                Process su = Runtime.getRuntime().exec("/system/bin/su");
+//                //一般的都是/system/bin/su路径，有的也是/system/xbin/su
+//                String cmd = "chmod 777 " + device.getAbsolutePath() + "\n" + "exit\n";
+//                su.getOutputStream().write(cmd.getBytes());
+//
+//                if ((su.waitFor() != 0) || !device.canRead() || !device.canWrite()) {
+//                    throw new SecurityException();
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                throw new SecurityException();
+//            }
+//        }
+
+        mSerialPort = new SerialPort(new File(sPort), iBaudRate, 0, mDataBits, mStopBits, mParityBits);
+        mOutputStream = mSerialPort.getOutputStream();
+        mInputStream = mSerialPort.getInputStream();
+        mBufferedInputStream = new BufferedInputStream(mInputStream);
+        mReadThread = new ReadThread();
+        mReadThread.start();
+        mSendThread = new SendThread();
+        mSendThread.setSuspendFlag();
+        mSendThread.start();
+        _isOpen = true;
+    }
+
+    //----------------------------------------------------
+    public void close() {
+        if (mReadThread != null)
+            mReadThread.interrupt();
+        if (mSerialPort != null) {
+            mSerialPort.close();
+            mSerialPort = null;
+        }
+        if (mInputStream != null) {
+            try {
+                mInputStream.close();
+                mInputStream = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (mOutputStream != null) {
+            try {
+                mOutputStream.close();
+                mOutputStream = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (mBufferedInputStream != null) {
+            try {
+                mBufferedInputStream.close();
+                mBufferedInputStream = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        _isOpen = false;
+    }
+
+    //----------------------------------------------------
+    public void send(byte[] bOutArray) {
+        try {
+            mOutputStream.write(bOutArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //----------------------------------------------------
+    public void sendHex(String sHex) {
+        byte[] bOutArray = MyFunc.HexToByteArr(sHex);
+        send(bOutArray);
+    }
+
+    //----------------------------------------------------
+    public void sendTxt(String sTxt) {
+        byte[] bOutArray = sTxt.getBytes();
+        send(bOutArray);
+    }
+
+
+    //----------------------------------------------------
+    private class ReadThread extends Thread {
+
+
+        @Override
+        public void run() {
+            super.run();
+            while (!isInterrupted()) {
+                try {
+                    if (mBufferedInputStream == null)
+                        mBufferedInputStream = new BufferedInputStream(mInputStream, 1024 * 64);
+                    byte[] buffer = new byte[60];
+                    int len;
+                    if (mBufferedInputStream.available() > 0) {
+                        try {
+                            sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        len = mBufferedInputStream.read(buffer);
+                        if (len != -1) {
+                            Log.e("buffer", "len:" + len + "值：" + MyFunc.ByteArrToHex(buffer));
+                            sendData(buffer,buffer.length);
+                        }
+                    }
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "run: 数据读取异常：" + e.toString());
+                    break;
+                }
+            }
+        }
+
+        private void sendData(byte[] bytes, int size) {
+            ComBean ComRecData = new ComBean(sPort, bytes, size);
+            if (mOnDataReceivedListener != null) {
+                mOnDataReceivedListener.onDataReceived(ComRecData);
+            }
+        }
+    }
+
+    //----------------------------------------------------
+    private class SendThread extends Thread {
+        public boolean suspendFlag = true;// 控制线程的执行
+
+        @Override
+        public void run() {
+            super.run();
+            while (!isInterrupted()) {
+                synchronized (this) {
+                    while (suspendFlag) {
+                        try {
+                            wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                send(getbLoopData());
+                Log.e(TAG, "sendSerialPort: 串口数据发送成功");
+                try {
+                    Thread.sleep(iDelay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //线程暂停
+        public void setSuspendFlag() {
+            this.suspendFlag = true;
+        }
+
+        //唤醒线程
+        public synchronized void setResume() {
+            this.suspendFlag = false;
+            notify();
+        }
+    }
+
+
+    //----------------------------------------------------
+    private Integer getArrayRealLength(byte[] bytes) {
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] == 0) {
+                return i;
+            }
+
+        }
+        return bytes.length;
+    }
+
+    public int getBaudRate() {
+        return iBaudRate;
+    }
+
+    public boolean setBaudRate(int iBaud) {
+        if (_isOpen) {
+            return false;
+        } else {
+            iBaudRate = iBaud;
+            return true;
+        }
+    }
+
+    public boolean setBaudRate(String sBaud) {
+        int iBaud = Integer.parseInt(sBaud);
+        return setBaudRate(iBaud);
+    }
+
+    //----------------------------------------------------
+    public String getPort() {
+        return sPort;
+    }
+
+    public boolean setPort(String sPort) {
+        if (_isOpen) {
+            return false;
+        } else {
+            this.sPort = sPort;
+            return true;
+        }
+    }
+
+    public String getDataBits() {
+        return mDataBits;
+    }
+
+    public boolean setDataBits(String dataBits) {
+        if (_isOpen) {
+            return false;
+        }else {
+            this.mDataBits = dataBits;
+            return true;
+        }
+    }
+
+    public String getStopBits() {
+        return mStopBits;
+    }
+
+    public boolean setStopBits(String stopBits) {
+        if (_isOpen) {
+            return false;
+        }else {
+            this.mStopBits = stopBits;
+            return true;
+        }
+    }
+
+    public String getParityBits() {
+        return mParityBits;
+    }
+
+    public boolean setParityBits(String parityBits) {
+        if (_isOpen) {
+            return false;
+        }else {
+            this.mParityBits = parityBits;
+            return true;
+        }
+    }
+
+    //----------------------------------------------------
+    public boolean isOpen() {
+        return _isOpen;
+    }
+
+    //----------------------------------------------------
+    public byte[] getbLoopData() {
+        return _bLoopData;
+    }
+
+    //----------------------------------------------------
+    public void setbLoopData(byte[] bLoopData) {
+        this._bLoopData = bLoopData;
+    }
+
+    //----------------------------------------------------
+    public void setTxtLoopData(String sTxt) {
+        this._bLoopData = sTxt.getBytes();
+    }
+
+    //----------------------------------------------------
+    public void setHexLoopData(String sHex) {
+        this._bLoopData = MyFunc.HexToByteArr(sHex);
+    }
+
+    //----------------------------------------------------
+    public int getiDelay() {
+        return iDelay;
+    }
+
+    //----------------------------------------------------
+    public void setiDelay(int iDelay) {
+        this.iDelay = iDelay;
+    }
+
+    //----------------------------------------------------
+    public void startSend() {
+        if (mSendThread != null) {
+            mSendThread.setResume();
+        }
+    }
+
+    //----------------------------------------------------
+    public void stopSend() {
+        if (mSendThread != null) {
+            mSendThread.setSuspendFlag();
+        }
+    }
+
+    //----------------------------------------------------
+    interface OnDataReceivedListener {
+        void onDataReceived(ComBean ComRecData);
+    }
+
+    private OnDataReceivedListener mOnDataReceivedListener;
+
+    public void setOnDataReceivedListener(OnDataReceivedListener onDataReceivedListener) {
+        mOnDataReceivedListener = onDataReceivedListener;
+    }
+}
